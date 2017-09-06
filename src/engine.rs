d37780e6504d9c848741463a229ba54768c5c313
Lakin Wecker <lakin@structuredabstraction.com>
2017-09-06 16:47:38 -0600

    initial simple engine for seb32

---
+++ src/engine.rs
@@ -0,0 +1,11 @@
+
+use shakmaty::fen::Fen;
+use shakmaty::{Chess, Position};/*, MoveList};*/
+
+pub fn search(board: &Chess) {
+    println!("I found nothing! Where is Seb?!");
+}
+
+pub fn eval(board: &Chess) {
+    println!("I have no idea what I'm doing! Where is Seb?!");
+}
